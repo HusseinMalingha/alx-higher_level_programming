b58740d79294db9b89c3dd1383a76c5fbef3a7c4
HusseinMalingha <husseinmalingha@gmail.com>
2024-01-08 11:22:32 -0800

    Added a script to print reversed list of a given list

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    reversed_list = my_list[::-1]
+    for i in reversed_list:
+        print("{:d}".format(i))
+
+
+if __name__ == "__main__":
+    print_reversed_list_integer(my_list)

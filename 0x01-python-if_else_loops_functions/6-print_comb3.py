82f306e46c9f07897165589a43508cceda29ed74
HusseinMalingha <husseinmalingha@gmail.com>
2024-02-13 09:22:20 -0800

    A program that prints a combination of two smaller digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 for i in range(0, 9):
-    for j in range(1, 10):
-        if (i,j) < (8,9):
-            if i != j:
-                print("{:d}{:d}, ".format(i, j), end="")
-        else:
-            print("{:d}{:d}".format(i, j))
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end=", " if i < 8 or j < 9 else "\n")

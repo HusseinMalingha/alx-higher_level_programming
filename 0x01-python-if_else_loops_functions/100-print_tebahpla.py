d2f5facd4c772964a5d460a4a2ecadd1132d991a
HusseinMalingha <husseinmalingha@gmail.com>
2024-02-15 02:00:19 -0800

    Reversed Alphabet with a Ab pattern

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_tebahpla():
-    for i in range(65, 91):
+    for i in reversed(range(65, 91,)):
         print("{}".format(chr(i + 32) if i % 2 == 0 else chr(i)), end="")
 
 

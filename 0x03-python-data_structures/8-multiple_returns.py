511d82ba3bb0d69b320b1019fdc0f0ac00975320
HusseinMalingha <husseinmalingha@gmail.com>
2024-02-15 04:48:55 -0800

    Tuple of length of string and its first chr

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence):
+        return len(sentence), sentence[0]
+    return 0, None
+
+
+if __name__ == "__main__":
+    multiple_returns(sentence)

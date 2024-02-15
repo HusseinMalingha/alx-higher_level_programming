c958ce552ec2130e40c9d2a84ef07e95c682f1c6
HusseinMalingha <husseinmalingha@gmail.com>
2024-02-15 04:19:00 -0800

    Adding Tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    if len(tuple_a) == 2 and len(tuple_b) == 2:
+        return (tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1])
+    elif len(tuple_a) == 1 and len(tuple_b) == 2:
+        return (tuple_a[0] + tuple_b[0]), (0 + tuple_b[1])
+    elif len(tuple_a) == 2 and len(tuple_b) == 1:
+        return (tuple_a[0] + tuple_b[0]), (tuple_a[1] + 0)
+    elif len(tuple_a) == 0:
+        return tuple_b
+    elif len(tuple_b) == 0:
+        return tuple_a
+
+
+if __name__ == "__main__":
+    add_tuple(tuple_a, tuple_b)
